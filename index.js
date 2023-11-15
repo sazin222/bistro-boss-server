90a9c0e82b24762e6a881f3a58813fdd73ecb312
sazin222 <mahadiislamsazin.com>
2023-11-15 10:55:43 +0600

    created the delete api by a id

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.na448cw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -43,7 +43,6 @@ async function run() {
     // get data from cart collection 
     app.get('/carts', async (req,res)=>{
       const email = req.query.email
-      console.log('email', email);
       const query ={email: email}
     const result = await cartsCollection.find(query).toArray()
        res.send(result)
@@ -53,6 +52,13 @@ async function run() {
      const cartItem= req.body 
      const result = await cartsCollection.insertOne(cartItem)
      res.send(result)
+  })
+
+  app.delete('/carts/:id', async(req,res)=>{
+    const id = req.params.id
+    const query = {_id: new ObjectId(id)}
+    const result = await cartsCollection.deleteOne(query)
+    res.send(result)
   })
 
 
