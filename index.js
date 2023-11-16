f279ecee82e08d5d6842f94e1b4574b399e90c70
sazin222 <mahadiislamsazin.com>
2023-11-16 19:27:53 +0600

    creted the middleware for verify token and verify admin

--- index.js
+++ index.js
@@ -39,12 +39,59 @@ async function run() {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
       res.send({token})
     })
+ 
+    // middleware 
+    const verifyToken = (req, res, next)=>{
+      console.log('inside verify token',
+      req.headers.authorization);
+      if(!req.headers.authorization){
+       return res.status(401).send({message:'unauthorized access'})
+      } 
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+        if(err){
+          return res.status(401).send({message:'unauthorized access'})
+        }
+        req.decoded= decoded
+
+        next()
+      })
+    } 
+
+    const verifyAdmin = async(req,res, next)=>{
+      const email= req.decoded.email
+      const query= {email: email} 
+      const user = await userCollection.findOne(query)
+      const isAdmin = user?.role === 'admin'
+      if(!isAdmin){
+        return res.status(401).send({message:'forbidden access'})
+      }
+      next()
+    }
+
+
   // user related api 
-  app.get('/users', async(req, res)=>{
+  app.get('/users', verifyToken, verifyAdmin, async(req, res)=>{
     console.log(req.headers);
     const result = await userCollection.find().toArray()
     res.send(result)
   })
+ 
+  app.get('/users/admin/:email', verifyToken, async (req, res)=>{
+    const email = req.params.email 
+    if(email !== req.decoded.email){
+      return req.status(403).send({message: 'forbidden access'})
+    } 
+    const query = {email: email}
+    const user = await userCollection.findOne(query)
+     let admin = false 
+     if(user){
+      admin = user?.role === 'admin'
+     }
+     res.send({admin})
+  })
+
+
   app.post('/users', async(req,res)=>{
     const user= req.body 
     // insert email if email does'nt exist
@@ -59,7 +106,7 @@ async function run() {
 
   })
   
-  app.patch('/users/admin/:id', async(req,res)=>{
+  app.patch('/users/admin/:id', verifyToken, verifyAdmin, async(req,res)=>{
      const id = req.params.id 
      const filter={_id: new ObjectId(id)}
 
@@ -73,7 +120,7 @@ async function run() {
   })
 
 
-   app.delete('/users/:id', async(req,res)=>{
+   app.delete('/users/:id', verifyToken, verifyAdmin, async(req,res)=>{
      const id= req.params.id
      const query= {_id: new ObjectId(id)} 
      const result = userCollection.deleteOne(query)
